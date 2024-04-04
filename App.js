a2196097241053a6084f728f420e26b74c0f9576
Alec Varma <alec_varma@yahoo.com>
2024-04-04 16:32:05 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+const app = express()
+app.use(cors());
+CourseRoutes(app);
+app.use(express.json());
+Hello(app)
+Lab5(app)
+app.listen(process.env.PORT || 4000)
